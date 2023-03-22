8ba67aba9baebcedae53b16739b843d6c05c5d92
Henry <ikegwuonuhenry25@gmail.com>
2023-03-22 03:44:03 +0000

    monty task

---
+++ rotr.c
@@ -0,0 +1,33 @@
+/*
+ * Auth: Philip Favour B.
+ * Col: Henry Ikegwuony
+ * File: rotr.c
+ */
+#include "monty.h"
+
+/**
+* f_rotr - function that rotates the stack to the bottom
+* @head: stack head of the stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *copy;
+
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
+}
